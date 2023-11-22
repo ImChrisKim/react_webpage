12751dfd71697fdd43a86af6c965169740299411
ChrisKim <chriskim9512@gmail.com>
2023-11-22 09:25:38 +0900

    header 작성 완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -56,7 +56,7 @@ const Header = () => {
           aria-controls="primary-menu"
           aria-expanded={show ? "true" : "false"}
           role="button"
-          tabindex="0"
+          tabIndex="0"
           onClick={toggleMenu}
         >
           <span></span>
